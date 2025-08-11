6eae0742b6dc064afd6bfc6fd26c46450445e0a0
ngandolh <nganttk002@gmail.com>
2025-08-12 00:47:36 +0700

    [Add][Javascript] Conditional statement

---
+++ Javascript/condition.js
@@ -0,0 +1,24 @@
+//Conditional Statements
+
+//1. if statement
+if (condition) {
+    // code to be executed if condition is true
+    console.log("12th August is history!!!");
+} else if (anotherCondition) {
+    console.log("Another condition is true!");
+} else {
+    console.log("It's holiday!");
+}
+
+
+//2. switch statement 
+switch (expersion) {
+    case "apple":
+        console.log("It's an apple!");
+        break;
+    case "banana":
+        console.log("It's a banana!");
+        break;
+    default:
+        console.log("Unknown fruit.");
+}
