4907bd3c82cedf19657aecc8825f002f86660a8d
ngandolh <nganttk002@gmail.com>
2025-08-11 23:12:02 +0700

    [Add][Javascript] Variable

---
+++ Javascript/variable.js
@@ -0,0 +1,17 @@
+// Variable
+
+//var
+var a = 7;
+var b = 12;
+
+//let
+let y = 14;
+//let y = 15; - Error
+y = 15; //Valid
+
+//const
+const z = 10;
+//const z = 11; - Error
+z = 11; //Error
+
+//let - const: are same block scope, cannot be re-declared
